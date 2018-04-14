e919baf1e8cdfc549bf984b49b345ccbe0f6e80a
aedobbyn <adobbs11@gmail.com>
2018-04-14 13:07:04 -0500

    use tryCatch for beep_on_error

--- beep_on_error.R
+++ beep_on_error.R
@@ -1,15 +1,30 @@
 library(tidyverse)
 library(beepr)
 
-beep_on_error <- function(f, ...) {
-  g <- purrr::possibly(f, otherwise = "Beep pls")
+beep_on_error <- function(inp, error_beep = 9) {
+  q_inp <- substitute(inp)
   
-  # tryCatch(f(...), error = beep(1)
-  # )
+  msg <- paste0("An error occurred in, deparse(q_inp)")
+  e <- simpleError(msg)
   
-  outcome <- g()
+  tryCatch(inp, error = function(e) {
+    message(paste0(msg, ": ", e$message))
+    beep(error_beep)
+    })
+}
 
-  if (outcome == "Beep pls") {
+beep_on_error(log("foo"))
+beep_on_error(log(3))
+
+log("foo") %>% beep_on_error()
+
+
+beep_on_error <- function(f, error_beep = 9, arg) {
+  g <- purrr::safely(f, otherwise = "Beep pls")
+  
+  outcome <- g(arg)
+  
+  if (outcome$result == "Beep pls") {
     beep(1)
   } else {
     beep(2)
@@ -17,7 +32,10 @@ beep_on_error <- function(f, ...) {
   return(NULL)
 }
 
-beep_on_error(log, "a")
+beep_on_error(log, arg = "foo")
+beep_on_error(log, arg = 1)
+
+log("a") %>% beep_on_error()
 
 beep_on_error(log(1))
 
