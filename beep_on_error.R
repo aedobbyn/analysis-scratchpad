0eb4f2da24e48ee9a6d04db71329f94fc899018c
aedobbyn <adobbs11@gmail.com>
2018-04-30 11:02:20 -0500

    two beep_on_errors, one using safely

--- beep_on_error.R
+++ beep_on_error.R
@@ -2,21 +2,23 @@ library(tidyverse)
 library(beepr)
 
 beep_on_error <- function(inp, error_beep = 9) {
-  q_inp <- substitute(inp)
-  
-  msg <- paste0("An error occurred in, deparse(q_inp)")
+  # q_inp <- substitute(inp)
+  # 
+  # msg <- paste0("An error occurred in ", deparse(q_inp))
+  msg <- "bar"
   e <- simpleError(msg)
   
   tryCatch(inp, error = function(e) {
     message(paste0(msg, ": ", e$message))
     beep(error_beep)
-    })
+  })
 }
 
-beep_on_error(log("foo"))
-beep_on_error(log(3))
+beep_on_error() %>% log("foo")
 
-log("foo") %>% beep_on_error()
+beep_on_error(log("foo"))
+
+beep_on_error(log(3))
 
 
 beep_on_error <- function(f, error_beep = 9, arg) {
